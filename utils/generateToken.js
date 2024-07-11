ac4eb514f049e2ccecf1922b484ab8b7d37baedc
Kneyx_Kodes <afomachukwuchinenyeonyekaba@gmail.com>
2024-07-11 14:59:10 +0100

    Updated generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (userId, email) => {
-  console.log(email);
-  console.log(userId);
   const token = jwt.sign({ userId, email }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
